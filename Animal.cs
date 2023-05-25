ebf12e45a1a26d3cc1d949fbed1bbe6e4bd88ac6
StarDestroyer66 <72447658+SavuStefan@users.noreply.github.com>
2023-05-25 13:47:30 +0300

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSounds
+{
+    internal class Animal
+    {
+        public virtual void SunetAnimal()
+        {
+            Console.WriteLine("Animal makes sound");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace AnimalSounds
+{
+    class Porc : Animal
+    {
+        public override void SunetAnimal()
+        {
+            Console.WriteLine("Porc makes: oink,oink.");
+        }
+    }
+
+    class Caine : Animal
+    {
+        public override void SunetAnimal()
+        {
+            Console.WriteLine("Câine makes: woof, woof");
+        }
+    }
+
+
+    class Pisica : Animal
+    {
+        public override void SunetAnimal()
+        {
+            Console.WriteLine("Pisică makes: miau.");
+        }
+    }
+
+
+    class Lup : Animal
+    {
+        public override void SunetAnimal()
+        {
+            Console.WriteLine("Delfin makes: WOOOOOF!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal caine = new Caine();
+            Animal porc = new Porc();
+            Animal lup = new Lup();
+            Animal pisica = new Pisica();
+
+            List<Animal> animale = new List<Animal>();
+            animale.Add(caine);
+            animale.Add(porc);
+            animale.Add(lup);
+            animale.Add(pisica);
+
+
+            foreach (Animal animal in animale)
+            {
+                animal.SunetAnimal();
+            }
+        }
+    }
+}
